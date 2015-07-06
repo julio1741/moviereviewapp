0a206a1ed92fe81f906b3b03f0a8de001422a288
Julio <julio@acid.cl>
2015-07-06 12:15:41 -0300

    add paper clip gem

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,5 +1,5 @@
 class Movie < ActiveRecord::Base
   belongs_to :user
-  has_attached_file :images, styles: { medium: "400x600#", thumb: "100x100>" }, default_url: "/images/style/missing.png"
-  validates_attachment_content_type :images, content_type: /\Aimage\/.*\Z/	
+  has_attached_file :image, styles: { medium: "400x600#", thumb: "100x100>" }, default_url: "/images/style/missing.png"
+  validates_attachment_content_type :image, content_type: /\Aimage\/.*\Z/	
 end
